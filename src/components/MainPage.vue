79eddf12688468a55341d51f5dca4a8b9679f3a9
C40986 <sudheer.berelly@nl.abnamro.com>
2020-09-11 01:22:03 +0200

    Initial commit

---
+++ src/components/MainPage.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="hello">
+    <Menu></Menu>
+    <div class="heading">
+      <h1>{{ msg }}</h1>
+    </div>
+    </div>
+</template>
+
+<script>
+  import Menu from "./menu-bar/Menu";
+export default {
+  name: 'MainPage',
+  components: {
+    Menu
+  },
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+  .heading{
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 20rem;
+  }
+</style>
